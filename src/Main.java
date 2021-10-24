d25553a1a85f4b60e27cc5cbc56a6b08e9393af8
DmitriyVlz <appattit@mail.com>
2021-10-24 17:03:06 +0300

    nigative total

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        long ballance = 2_000_000_000;
-        long transfer = 500_000_000;
-        long total = ballance * transfer;
+        int ballance = 2_000_000_000;
+        int transfer = 500_000_000;
+        int total = ballance * transfer;
         System.out.println(total);
 
     }
